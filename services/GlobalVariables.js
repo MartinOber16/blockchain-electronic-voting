1ec4abd7c5f52c0e767accc9553d9801665f1b49
Martin Ober <martinober@MacBook-Air-de-Martin.local>
2020-07-03 22:08:31 -0300

    20200703

--- services/GlobalVariables.js
+++ services/GlobalVariables.js
@@ -1,6 +1,6 @@
 export const okCode = 200;
 export const errorCode = 204;
 export const networkName = 'Rinkeby Testnet';
-export const ethersForVoter = 100000000000000; // 0,0001 etherss
+export const ethersForVoter = 200000000000000; // 0,0002 etherss
 export const smartContractVersion = '0.9.1';
 export const appVersion = '0.9.2';
